714b1f8575c023e92bf64ec03fe37ffa5b2b761a
Sulaiman Alajlan <salajlan@sfh.med.sa>
2015-10-27 14:55:07 +0300

    Add helper and Query

---
+++ CamundaCSharpClient/Helper/EnsureHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CamundaCSharpClient.Helper
+{
+    public class EnsureHelper
+    {
+        public void ensureNotNull(string variableName, object value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(variableName, "Exception has been thrown due to null value");
+            }
+        }
+    }
+}
---
+++ CamundaCSharpClient/Helper/queryHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CamundaCSharpClient.Helper
+{
+    public class queryHelper
+    {
+        public string buildQuery<T>(T query)
+        {
+            string queryString = null;
+            foreach (var item in query.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                if (item.GetValue(query, null) == null) continue;
+                queryString += item.Name + "=" + item.GetValue(query, null) + "&";
+            }
+            return queryString;
+        }
+    }
+}
