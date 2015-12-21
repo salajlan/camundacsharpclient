1e75d9f75fa545f136ce8d515c3fea189c4fa253
Sulaiman Alajlan <salajlan@sfh.med.sa>
2015-12-21 16:57:32 +0300

    Document ensure and return Helper classes

--- CamundaCSharpClient/Helper/EnsureHelper.cs
+++ CamundaCSharpClient/Helper/EnsureHelper.cs
@@ -7,6 +7,11 @@ namespace CamundaCSharpClient.Helper
 {
     public static class EnsureHelper
     {
+        /// <summary>
+        /// ensure the varible is not null and if it's  throw argument null exception.
+        /// </summary>
+        /// <param name="variableName"> the variable name to print in the exception message </param>
+        /// <param name="value">value to check if null</param>
         public static void NotNull(string variableName, object value)
         {
             if (value == null)
--- CamundaCSharpClient/Helper/ReturnHelper.cs
+++ CamundaCSharpClient/Helper/ReturnHelper.cs
@@ -10,6 +10,14 @@ namespace CamundaCSharpClient.Helper
 {
     public class ReturnHelper
     {
+        /// <summary>
+        /// when the camunda REST API return is no content status (204) we will call this methos to deserlize the content of the request if it
+        /// has content that means that request has an error so we have to descerlize it and send it as CamundaBase if its null then 
+        /// send it as normal
+        /// </summary>
+        /// <param name="content">Request content</param>
+        /// <param name="statusCode"> status Code of the request</param>
+        /// <returns>NoContentStatus</returns>
         public static NoContentStatus NoContentReturn(string content, HttpStatusCode statusCode)
         {
             var desc = JsonConvert.DeserializeObject<CamundaBase>(content);
